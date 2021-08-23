9423669318458f388e1a7b9946d61f6c512e778d
ibsenriou <ibsenriou@hotmail.com>
2021-08-23 16:48:24 -0300

    Conclusao da aula03

--- usuarios/views.py
+++ usuarios/views.py
@@ -78,31 +78,6 @@ def dashboard(request):
         return redirect('index')
 
 
-def cria_receita(request):
-    if request.method == 'POST':
-        nome_receita = request.POST['nome_receita']
-        ingredientes = request.POST['ingredientes']
-        modo_preparo = request.POST['modo_preparo']
-        tempo_preparo = request.POST['tempo_preparo']
-        rendimento = request.POST['rendimento']
-        categoria = request.POST['categoria']
-        foto_receita = request.FILES['foto_receita']
-        user = get_object_or_404(User, pk=request.user.id)
-        receita = Receita.objects.create(pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes,
-                                         modo_preparo=modo_preparo, tempo_preparo=tempo_preparo,
-                                         rendimento=rendimento, categoria=categoria, foto_receita=foto_receita)
-        receita.save()
-        return redirect('dashboard')
-    else:
-        return render(request, 'usuarios/cria_receita.html')
-
-
-def deleta_receita(request, receita_id):
-    receita = get_object_or_404(Receita, pk=receita_id)
-    receita.delete()
-    return redirect('dashboard')
-
-
 def campo_vazio(campo):
     return not campo.strip()
 
